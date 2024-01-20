78c67cbcf454b9fd9b2d1c7f0773968dca9f875a
Suresh-23723 <21203048@rmd.ac.in>
2024-01-20 20:26:47 +0530

    Finish project

--- public/javascripts/fileUpload.js
+++ public/javascripts/fileUpload.js
@@ -1,7 +1,31 @@
-Filepond.registerPlugin(
-    FilePondPluginImagePreview,
-    FilePondPluginImageResize,
-    FilePondPluginFileEncode,
+const rootStyles = window.getComputedStyle(
+    document.documentElement
 )
 
-FilePond.parse(document.body);
+if(rootStyles.getPropertyValue
+    ('--book-cover-width-large') != null && ('--book-cover-width-large') != '') {
+        ready()
+    }
+else {
+    document.getElementById('main-css')
+    .addEventListener('load',ready)
+}
+
+
+function ready() {
+    const coverWidth = parseFloat(rootStyles.getPropertyValue('--book-cover-width-large'))
+    const coverAspectRatio = parseFloat(rootStyles.getPropertyValue('--book-cover-aspect-ratio'))
+    const coverHeight = coverWidth / coverAspectRatio
+    Filepond.registerPlugin(
+        FilePondPluginImagePreview,
+        FilePondPluginImageResize,
+        FilePondPluginFileEncode,
+    )
+    
+    FilePond.setOptions({
+        stylePanelAspectRatio: 1 / coverAspectRatio,
+        imageResizeTargetWidth: coverWidth,
+        imageResizeTargetHeight: coverHeight
+    })
+    FilePond.parse(document.body);
+}
